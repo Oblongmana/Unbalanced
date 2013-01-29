f745f5898d55fcedfdf2e14864b3be1544bbf8c3
James Hill <oblongmana@gmail.com>
2013-01-29 22:23:44 +1300

    Initial Commit
    Fully functional

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Unbalanced
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ofd1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 103);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(968, 358);
+            this.textBox1.TabIndex = 0;
+            // 
+            // ofd1
+            // 
+            this.ofd1.FileName = "openFileDialog1";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(568, 58);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(200, 23);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Select CSV";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(774, 58);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(206, 23);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "Determine Possible Balance Paths";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 473);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog ofd1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GenericParsing;
+
+namespace Unbalanced
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static void BuildPaths(List<List<String>> Operations, List<String> pathSoFar, int count, int stop)
+        {
+            if (count < stop)
+            {
+                pathSoFar.Add("+");
+                BuildPaths(Operations,new List<String>(pathSoFar), count + 1, stop);
+
+                pathSoFar.RemoveAt(count);
+                pathSoFar.Add("-");
+                BuildPaths(Operations,new List<String>(pathSoFar), count + 1, stop);
+            }
+            else
+            {
+                Operations.Add(pathSoFar);
+            }
+        }
+
+        private static void SumPaths(List<Decimal> DecimalsToSum, List<Decimal> Sums, Decimal sumSoFar, int count, int stop)
+        {
+            if (count < stop)
+            {
+                sumSoFar += DecimalsToSum[count];
+                SumPaths(DecimalsToSum, Sums, sumSoFar, count + 1, stop);
+
+                sumSoFar -= (DecimalsToSum[count]*2);
+                SumPaths(DecimalsToSum, Sums, sumSoFar, count + 1, stop);
+            }
+            else
+            {
+                Sums.Add(sumSoFar);
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = ofd1.ShowDialog();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            //ArgumentParser argParser = new ArgumentParser(args);
+
+            String dataSource = ofd1.FileName;//argParser["o"];
+
+            if (dataSource != null)
+            {
+                try
+                {
+                    GenericParser parser = new GenericParser();
+                    parser.SetDataSource(dataSource);
+                    parser.FirstRowHasHeader = true;
+                    parser.Read();
+
+                    int columnCount = parser.ColumnCount;
+                    string[] ItemNames = new string[columnCount];
+                    for (int i = 0; i < columnCount; i++)
+                    {
+                        ItemNames[i] = parser.GetColumnName(i);
+                    }
+                    List<List<String>> Operations = new List<List<String>>();
+                    BuildPaths(Operations, new List<String>(), 0, columnCount);
+
+                    List<Decimal> DecimalsToSum = new List<decimal>();
+                    List<Decimal> Sums = new List<decimal>();
+                    for (int decimalCounter = 0; decimalCounter < columnCount; decimalCounter++)
+                    {
+                        DecimalsToSum.Add(Decimal.Parse(parser[decimalCounter]));
+                    }
+                    SumPaths(DecimalsToSum, Sums, 0, 0, columnCount);
+
+                    String dump = "";
+                    int countSuccess = 0;
+                    for (int i = 0; i < Operations.Count; i++)
+                    {
+                        if (Sums[i] == 0)
+                        {
+                            countSuccess++;
+                            int innerCount = Operations[i].Count;
+                            //dump += '(';
+                            for (int j = 0; j < innerCount; j++)
+                            {
+                                //dump += ("[" + Operations[i][j] + parser.GetColumnName(j) + ":" + DecimalsToSum[j] + "]");
+                                dump += (parser.GetColumnName(j).PadLeft(30) + ":" + DecimalsToSum[j].ToString().PadLeft(15).PadRight(4) + "(" + Operations[i][j] + ")" + Environment.NewLine);
+                            }
+                            dump += "Balances To".PadLeft(30) + ":" + Sums[i].ToString().PadLeft(15) + Environment.NewLine + Environment.NewLine;
+                            //dump += ("    BalancesTo:" + Sums[i] + ')' + Environment.NewLine);
+                        }
+                    }
+
+                    if (countSuccess > 0) textBox1.Text = dump;
+                    else textBox1.Text = "This combination of numbers has no possible way to balance";
+                }
+                catch (Exception ex)
+                {
+                    textBox1.Text = ex.Message;
+                }
+
+                
+            }    
+        }
+
+        
+    }
+
+    
+}
