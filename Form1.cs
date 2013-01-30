ced242a9243a93fd5e8783b8739b5f6312803f42
James Hill <james.hill@snap.net.nz>
2013-01-30 15:46:58 +1300

    Chop old code; Comment recursion; Console writes
    Chopped out old code (2x old recursive methods, and their associated
    processing/display code).
    
    Comment on the recursive method so it hopefully makes a bit of sense if
    I look at it in the future or someone else looks at it.
    
    Added Console.Writes wherever we write to the textBox, just in case you
    want it in the console. Which I do

--- Form1.cs
+++ Form1.cs
@@ -16,65 +16,46 @@ namespace Unbalanced
 		{
 			InitializeComponent();
 		}
-		
-		private static void BuildPathsAndSumsTogether (List<Tuple<Decimal,List<String>>> SumsAndOpps, List<Tuple<Decimal,Decimal,List<String>>> AbsSumsAndOpps, List<Decimal> DecimalsToSum, List<String> oppsSoFar, Decimal sumSoFar, int count, int stop)
+
+		/// <summary>
+		/// Recursively run through every single way to add/subtract the numbers together, producing a List describing the different
+		/// possible balances, and the operations conducted to get said balance. Brute-force, with the sole enhancement being that if 
+		/// one of the numbers is Zero, then instead of branching down both the Add and Subtract paths, will go down a single 
+		/// "Plus-Minus" path, bringing this algorithm to 2^(n - [Count of Accounts with Zero balance])
+		/// 
+		/// </summary>
+		/// <param name="SumsAndOps">List of Tuples, where each Tuple represents a Sum and the Operations conducted to obtain the sum.</param>
+		/// <param name="AbsSumsAndOps">As for SumsAndOps, but the Tuple has an additional item (item 1) which is the absolute value of the Sum</param>
+		/// <param name="DecimalsToSum">The numbers we are attempting to balance to Zero.</param>
+		/// <param name="oppsSoFar">The operations conducted so far.</param>
+		/// <param name="sumSoFar">The Sum of all operations so far.</param>
+		/// <param name="count">How many of the numbers we're attempting to balance that we've got through so far.</param>
+		/// <param name="stop">At what point the recursive method stops (this should be set to the sidze of the DecimalsToSum list).</param>
+		private static void BuildPathsAndSumsTogether (List<Tuple<Decimal,List<String>>> SumsAndOps, List<Tuple<Decimal,Decimal,List<String>>> AbsSumsAndOps, List<Decimal> DecimalsToSum, List<String> oppsSoFar, Decimal sumSoFar, int count, int stop)
 		{
 			if (count < stop)
-			{
+			{ 
 				Decimal amount = DecimalsToSum[count];
 				if(amount == 0){
 					oppsSoFar.Add("+-");
 					sumSoFar += 0;
-					BuildPathsAndSumsTogether(SumsAndOpps, AbsSumsAndOpps, DecimalsToSum, new List<String>(oppsSoFar), sumSoFar, count + 1, stop);
+					BuildPathsAndSumsTogether(SumsAndOps, AbsSumsAndOps, DecimalsToSum, new List<String>(oppsSoFar), sumSoFar, count + 1, stop);
 				}
 				else {
 					oppsSoFar.Add("+");
 					sumSoFar += amount;
-					BuildPathsAndSumsTogether(SumsAndOpps, AbsSumsAndOpps, DecimalsToSum, new List<String>(oppsSoFar), sumSoFar, count + 1, stop);
+					BuildPathsAndSumsTogether(SumsAndOps, AbsSumsAndOps, DecimalsToSum, new List<String>(oppsSoFar), sumSoFar, count + 1, stop);
 					
 					oppsSoFar.RemoveAt(count);
 					oppsSoFar.Add("-");
 					sumSoFar -= (amount*2);
-					BuildPathsAndSumsTogether(SumsAndOpps, AbsSumsAndOpps, DecimalsToSum, new List<String>(oppsSoFar), sumSoFar, count + 1, stop);
+					BuildPathsAndSumsTogether(SumsAndOps, AbsSumsAndOps, DecimalsToSum, new List<String>(oppsSoFar), sumSoFar, count + 1, stop);
 				}
 			}
 			else
 			{
-				SumsAndOpps.Add(new Tuple<decimal, List<string>>(sumSoFar,oppsSoFar));
-				AbsSumsAndOpps.Add(new Tuple<decimal, decimal, List<string>>(Math.Abs(sumSoFar), sumSoFar, oppsSoFar));
-			}
-		}
-		
-		private static void BuildPaths(List<List<String>> Operations, List<String> pathSoFar, int count, int stop)
-		{
-			if (count < stop)
-			{
-				pathSoFar.Add("+");
-				BuildPaths(Operations,new List<String>(pathSoFar), count + 1, stop);
-				
-				pathSoFar.RemoveAt(count);
-				pathSoFar.Add("-");
-				BuildPaths(Operations,new List<String>(pathSoFar), count + 1, stop);
-			}
-			else
-			{
-				Operations.Add(pathSoFar);
-			}
-		}
-		
-		private static void SumPaths(List<Decimal> DecimalsToSum, List<Decimal> Sums, Decimal sumSoFar, int count, int stop)
-		{
-			if (count < stop)
-			{
-				sumSoFar += DecimalsToSum[count];
-				SumPaths(DecimalsToSum, Sums, sumSoFar, count + 1, stop);
-				
-				sumSoFar -= (DecimalsToSum[count]*2);
-				SumPaths(DecimalsToSum, Sums, sumSoFar, count + 1, stop);
-			}
-			else
-			{
-				Sums.Add(sumSoFar);
+				SumsAndOps.Add(new Tuple<decimal, List<string>>(sumSoFar,oppsSoFar));
+				AbsSumsAndOps.Add(new Tuple<decimal, decimal, List<string>>(Math.Abs(sumSoFar), sumSoFar, oppsSoFar));
 			}
 		}
 		
@@ -114,123 +95,57 @@ namespace Unbalanced
 						DecimalsToSum.Add(Decimal.Parse(parser[decimalCounter]));
 					}
 					
-					List<Tuple<Decimal,List<String>>> SumsAndOpps = new List<Tuple<Decimal,List<String>>>();
-					List<Tuple<Decimal,Decimal,List<String>>> AbsSumsAndOpps = new List<Tuple<Decimal,Decimal,List<String>>>();
-					List<String> OppsSoFar = new List<string>();
+					List<Tuple<Decimal,List<String>>> SumsAndOps = new List<Tuple<Decimal,List<String>>>();
+					List<Tuple<Decimal,Decimal,List<String>>> AbsSumsAndOps = new List<Tuple<Decimal,Decimal,List<String>>>();
+					List<String> OpsSoFar = new List<string>();
 					
-					BuildPathsAndSumsTogether (SumsAndOpps, AbsSumsAndOpps, DecimalsToSum, OppsSoFar, 0, 0, columnCount);
-					SumsAndOpps.Sort( (a,b) => a.Item1.CompareTo(b.Item1) );
-					AbsSumsAndOpps.Sort( (a,b) => a.Item1.CompareTo(b.Item1) );
+					BuildPathsAndSumsTogether (SumsAndOps, AbsSumsAndOps, DecimalsToSum, OpsSoFar, 0, 0, columnCount);
+					SumsAndOps.Sort( (a,b) => a.Item1.CompareTo(b.Item1) );
+					AbsSumsAndOps.Sort( (a,b) => a.Item1.CompareTo(b.Item1) );
 					
 					int countSuccess = 0;
 					textBox1.Text = "";
-					foreach(Tuple<Decimal,List<string>> sumWithOpps in SumsAndOpps) {
-						if(sumWithOpps.Item1 == 0) {
+					foreach(Tuple<Decimal,List<string>> sumWithOps in SumsAndOps) {
+						if(sumWithOps.Item1 == 0) {
 							countSuccess++;
 							
 							for(int account = 0; account < ItemNames.Count; account++) {
 								textBox1.Text += (ItemNames[account].PadLeft(30) + ":" 
 								                  + DecimalsToSum[account].ToString().PadLeft(15).PadRight(4) 
-								                  + "(" + sumWithOpps.Item2[account] + ")" 
+								                  + "(" + sumWithOps.Item2[account] + ")" 
 								                  + Environment.NewLine);
+								Console.WriteLine(ItemNames[account]+":"+DecimalsToSum[account]+":"+sumWithOps.Item2[account]);
 							}
 							textBox1.Text += ("Balances To".PadLeft(30) + ":" 
-							                  + sumWithOpps.Item1.ToString().PadLeft(15) 
+							                  + sumWithOps.Item1.ToString().PadLeft(15) 
 							                  + Environment.NewLine + Environment.NewLine);
+							Console.WriteLine("Balances To" +":"+ sumWithOps.Item1);
+							Console.WriteLine();
 						}
 					}
 					
 					if(countSuccess == 0) {
-						int n = 8;
+						int n = 100;
 						
 						textBox1.Text = "This combination of numbers has no possible way to balance. The " + n + " solutions closest to 0 follow." + Environment.NewLine;
 						
-						for(int closeResultPos = 0 ; closeResultPos < n; closeResultPos++) {
-							Tuple<Decimal,Decimal,List<string>> sumWithOpps = AbsSumsAndOpps[closeResultPos];
+						for(int closeResultPos = 0 ; closeResultPos < n || closeResultPos >= AbsSumsAndOps.Count; closeResultPos++) {
+							Tuple<Decimal,Decimal,List<string>> sumWithOps = AbsSumsAndOps[closeResultPos];
 							for(int account = 0; account < ItemNames.Count; account++) {
 								textBox1.Text += (ItemNames[account].PadLeft(30) + ":" 
 								                  + DecimalsToSum[account].ToString().PadLeft(15).PadRight(4) 
-								                  + "(" + sumWithOpps.Item3[account] + ")" 
+								                  + "(" + sumWithOps.Item3[account] + ")" 
 								                  + Environment.NewLine);
+								Console.WriteLine(ItemNames[account]+":"+DecimalsToSum[account]+":"+sumWithOps.Item3[account]);
 							}
+
 							textBox1.Text += ("Balances To".PadLeft(30) + ":" 
-							                  + sumWithOpps.Item2.ToString().PadLeft(15) 
+							                  + sumWithOps.Item2.ToString().PadLeft(15) 
 							                  + Environment.NewLine + Environment.NewLine);
+							Console.WriteLine("Balances To" +":"+ sumWithOps.Item2);
+							Console.WriteLine();
 						}
 					}
-					
-					/*List<List<String>> Operations = new List<List<String>>();
-                    BuildPaths(Operations, new List<String>(), 0, columnCount);
-
-                    List<Decimal> DecimalsToSum = new List<decimal>();
-                    List<Decimal> Sums = new List<decimal>();
-                    for (int decimalCounter = 0; decimalCounter < columnCount; decimalCounter++)
-                    {
-                        DecimalsToSum.Add(Decimal.Parse(parser[decimalCounter]));
-                    }
-                    SumPaths(DecimalsToSum, Sums, 0, 0, columnCount);
-
-                    String dump = "";
-                    int countSuccess = 0;
-                    for (int i = 0; i < Operations.Count; i++)
-                    {
-                        if (Sums[i] == 0)
-                        {
-                            countSuccess++;
-                            int innerCount = Operations[i].Count;
-                            
-                            for (int j = 0; j < innerCount; j++)
-                            {
-                                dump += (parser.GetColumnName(j).PadLeft(30) + ":" + DecimalsToSum[j].ToString().PadLeft(15).PadRight(4) + "(" + Operations[i][j] + ")" + Environment.NewLine);
-                            }
-                            dump += "Balances To".PadLeft(30) + ":" + Sums[i].ToString().PadLeft(15) + Environment.NewLine + Environment.NewLine;
-                        }
-                    }
-
-                    if (countSuccess > 0) textBox1.Text = dump;
-                    else {
-						int n = 4;
-
-						textBox1.Text = "This combination of numbers has no possible way to balance. The " + n + " solutions closest to 0 follow." + Environment.NewLine;
-
-
-						int actualFoundCount = 0;
-						List<Decimal> lowestNSums = new List<decimal>();
-						List<Decimal> sumsCpy = new List<decimal>(Sums);
-
-						SortedDictionary<Decimal,List<int>> absSumToPositions = new SortedDictionary<decimal, List<int>>();
-						for(int position = 0 ; position < Sums.Count; position++) {
-							List<int> value = new List<int>();
-							Decimal absValue = Math.Abs(Sums[position]);
-							if(absSumToPositions.TryGetValue(absValue, out value)) {
-								value.Add(position);
-							}
-							else {
-								absSumToPositions.Add(absValue,new List<int>(position));
-							}
-						}
-
-						List<int> positionOfOutputItems = new List<int>();
-						int countItemsOutput = 0;
-						foreach(KeyValuePair<Decimal, List<int>> item in absSumToPositions)
-						{
-							foreach(int pos in item.Value) {
-								if(countItemsOutput >= n) break;
-								positionOfOutputItems.Add(pos);
-								countItemsOutput++;
-							}
-							if(countItemsOutput >= n) break;
-						}
-
-						foreach(int posToOutput in positionOfOutputItems) {
-							for (int j = 0; j < Operations[posToOutput].Count; j++)
-							{
-								textBox1.Text += (parser.GetColumnName(j).PadLeft(30) + ":" + DecimalsToSum[j].ToString().PadLeft(15).PadRight(4) + "(" + Operations[posToOutput][j] + ")" + Environment.NewLine);
-							}
-							textBox1.Text += "Balances To".PadLeft(30) + ":" + Sums[posToOutput].ToString().PadLeft(15) + Environment.NewLine + Environment.NewLine;
-						}
-
-					}*/
 				}
 				catch (Exception ex)
 				{
